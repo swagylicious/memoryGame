ebe7dce725a2eee68f924c9207e4bfb749cb3f0a
swagylicious <jflores23@nait.ca>
2024-05-06 20:50:18 +0000

    May 6, 2024, 2:50 PM

--- js/main.js
+++ js/main.js
@@ -6,6 +6,50 @@ var shullfeLegends = legends.sort(() => (Math.random() > 0.5) ? 2 : -1);
 
 console.log(shullfeLegends);
 
+// for ( var i = 0; i < legends.length; i++){
+//     const card = document.createElement('div');
+//     card.className = 'card';
+//     card.innerHTML = shullfeLegends[i];
+
+//     card.onclick = function(){
+//         this.classList.add('flip');
+//         setTimeout(function(){
+//             if(document.querySelectorAll('.flip').length < 2){
+
+//                 if(document.querySelectorAll('.flip').length == 2){
+//                     Array.from(document.querySelectorAll('.card')).forEach(card => {
+//                         card.onclick = null;
+//                     });
+    
+//                 if(document.querySelectorAll('.flip')[0].innerHTML == document.querySelectorAll('.flip')[1].innerHTML){
+
+//                     document.querySelectorAll('.flip')[0].classList.add('match');
+
+//                     document.querySelectorAll('.flip')[1].classList.add('match');
+
+//                     document.querySelectorAll('.flip')[0].classList.remove('flip');    
+                    
+//                     document.querySelectorAll('.flip')[1].classList.remove('flip');
+
+//                     if(document.querySelectorAll('.match').length == legends.length){
+//                         alert('You Win!');
+//                     }
+//                 }else{
+//                     document.querySelectorAll('.flip')[0].classList.remove('flip');    
+                    
+//                     document.querySelectorAll('.flip')[1].classList.remove('flip');
+//                 }
+//                 Array.from(document.querySelectorAll('.card')).forEach(card => {
+//                     card.onclick = arguments.callee;
+//                 });
+//             }
+            
+//         },500);
+//     };
+
+//     document.querySelector('.game').appendChild(card);
+// }
+
 for ( var i = 0; i < legends.length; i++){
     const card = document.createElement('div');
     card.className = 'card';
@@ -13,40 +57,26 @@ for ( var i = 0; i < legends.length; i++){
 
     card.onclick = function(){
         this.classList.add('flip');
-        setTimeout(function(){
-            if(document.querySelectorAll('.flip').length < 2){
-                
-                if(document.querySelectorAll('.flip').length == 2){
-                    Array.from(document.querySelectorAll('.card')).forEach(card => {
-                        card.onclick = null;
-                    });
-    
+        if(document.querySelectorAll('.flip').length == 2){
+            Array.from(document.querySelectorAll('.card')).forEach(card => {
+                card.onclick = null;
+            });
+            setTimeout(function(){
                 if(document.querySelectorAll('.flip')[0].innerHTML == document.querySelectorAll('.flip')[1].innerHTML){
-
                     document.querySelectorAll('.flip')[0].classList.add('match');
-
                     document.querySelectorAll('.flip')[1].classList.add('match');
-
-                    document.querySelectorAll('.flip')[0].classList.remove('flip');    
-                    
-                    document.querySelectorAll('.flip')[1].classList.remove('flip');
-
-                    if(document.querySelectorAll('.match').length == legends.length){
-                        alert('You Win!');
-                    }
-                }else{
-                    document.querySelectorAll('.flip')[0].classList.remove('flip');    
-                    
-                    document.querySelectorAll('.flip')[1].classList.remove('flip');
+                }
+                document.querySelectorAll('.flip')[0].classList.remove('flip');    
+                document.querySelectorAll('.flip')[1].classList.remove('flip');
+                if(document.querySelectorAll('.match').length == legends.length){
+                    alert('You Win!');
                 }
                 Array.from(document.querySelectorAll('.card')).forEach(card => {
                     card.onclick = arguments.callee;
                 });
-            }
-            
-        },500);
+            },500);
+        }
     };
 
     document.querySelector('.game').appendChild(card);
 }
-
