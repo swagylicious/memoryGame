de3cc3890be8fe21828210fb5e1440f40e42a45e
swagylicious <jflores23@nait.ca>
2024-05-06 19:58:17 +0000

    May 6, 2024, 1:58 PM

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ for ( var i = 0; i < legends.length; i++){
     card.className = 'card';
     card.innerHTML = shullfeLegends[i];
 
-    card = () => {
+    card.onclick = () => {
         
     };
 
