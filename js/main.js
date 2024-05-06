69b398b0eb1afcbcb5b37dab0b8e6f112d166cec
swagylicious <jflores23@nait.ca>
2024-05-06 19:34:31 +0000

    May 6, 2024, 1:34 PM

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@ const legends = ["😎","😎","👌","👌","😁","😁","🤡","🤡","👻",
 var shullfeLegends = legends.sort(() => (Math.random() > 0.5) ? 2 : -1);
 
 for ( var i = 0; i > legends.length; i++){
-    
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.name = legends[i];
+
+    const front = document.createElement('div');
+    front.classList.add('front');
+
+    const back = document.createElement('div');
+    back.classList.add('back');
+    back.style.backgroundImage = `url(img/${legends[i]}.png)`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    document.querySelector('.game-board').appendChild(card);
 }
