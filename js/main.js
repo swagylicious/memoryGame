fdd2f9cba6628cdcaaf7e697b88c8bfe8d1b50dc
swagylicious <jflores23@nait.ca>
2024-05-06 20:43:43 +0000

    May 6, 2024, 2:43 PM

--- js/main.js
+++ js/main.js
@@ -39,30 +39,6 @@ for ( var i = 0; i < legends.length; i++){
     //     },500);
     // };
 
-    card.onclick = function(){
-        if(document.querySelectorAll('.flip').length < 2){
-            this.classList.add('flip');
-            if(document.querySelectorAll('.flip').length == 2){
-                Array.from(document.querySelectorAll('.card')).forEach(card => {
-                    card.onclick = null;
-                });
-                setTimeout(function(){
-                    if(document.querySelectorAll('.flip')[0].innerHTML == document.querySelectorAll('.flip')[1].innerHTML){
-                        document.querySelectorAll('.flip')[0].classList.add('match');
-                        document.querySelectorAll('.flip')[1].classList.add('match');
-                    }
-                    document.querySelectorAll('.flip')[0].classList.remove('flip');    
-                    document.querySelectorAll('.flip')[1].classList.remove('flip');
-                    if(document.querySelectorAll('.match').length == legends.length){
-                        alert('You Win!');
-                    }
-                    Array.from(document.querySelectorAll('.card')).forEach(card => {
-                        card.onclick = arguments.callee;
-                    });
-                },500);
-            }
-        }
-    };
 
     document.querySelector('.game').appendChild(card);
 }
