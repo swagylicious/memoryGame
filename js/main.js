3d2791f0bc85184c74ae013b6444231594d9a5ab
swagylicious <jflores23@nait.ca>
2024-05-06 20:57:16 +0000

    May 6, 2024, 2:57 PM

--- js/main.js
+++ js/main.js
@@ -15,7 +15,15 @@ for ( var i = 0; i < legends.length; i++){
         this.classList.add('flip');
         setTimeout(function(){
           if(document.querySelectorAll('.flip').length > 1){
-            
+            if(document.querySelectorAll('.flip')[0].innerHTML !== document.querySelectorAll('.flip')[1].innerHTML){
+                document.querySelectorAll('.flip').forEach(function(card){
+                    card.classList.remove('flip');
+                });
+            }else{
+                document.querySelectorAll('.flip').forEach(function(card){
+                    card.classList.add('found');
+                    card.onclick = null;
+                });
           }
         },500);
     }       
