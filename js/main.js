56c6900459487a88690ee3632bf69c6ad50e8517
swagylicious <jflores23@nait.ca>
2024-05-06 20:56:03 +0000

    May 6, 2024, 2:56 PM

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ for ( var i = 0; i < legends.length; i++){
     card.onclick = function(){
         this.classList.add('flip');
         setTimeout(function(){
-          if(document.querySelectorAll('.flip').length === 2){
+          if(document.querySelectorAll('.flip').length < 2>){
             const [firstCard, secondCard] = document.querySelectorAll('.flip');
             if(firstCard.innerHTML !== secondCard.innerHTML){
                 firstCard.classList.remove('flip');
