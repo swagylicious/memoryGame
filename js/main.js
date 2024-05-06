eeac21b4f99c0d8bd48209565f3587527db83885
swagylicious <jflores23@nait.ca>
2024-05-06 20:59:04 +0000

    May 6, 2024, 2:59 PM

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ for ( var i = 0; i < legends.length; i++){
         this.classList.add('flip');
         setTimeout(function(){
           if(document.querySelectorAll('.flip').length > 1){
-                if(document.querySelectorAll('.flip')[0].innerHTML == document.querySelectorAll){}
+                if(document.querySelectorAll('.flip')[0].innerHTML == document.querySelectorAll('.flip')){}
             }
         },500);
     }       
