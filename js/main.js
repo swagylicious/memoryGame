f9b6384efc3c7f0c658d3c951332e94a58dc3078
swagylicious <jflores23@nait.ca>
2024-05-06 20:42:30 +0000

    May 6, 2024, 2:42 PM

--- js/main.js
+++ js/main.js
@@ -36,22 +36,6 @@ for ( var i = 0; i < legends.length; i++){
             //     }
             // }
             
-            if(document.querySelectorAll('.flip')[0].innerHTML == document.querySelectorAll('.flip')[1].innerHTML){
-                document.querySelectorAll('.flip')[0].classList.add('match');
-                document.querySelectorAll('.flip')[1].classList.add('match');
-            }
-            
-            document.querySelectorAll('.flip')[0].classList.remove('flip');    
-            
-            document.querySelectorAll('.flip')[1].classList.remove('flip');
-            
-            if(document.querySelectorAll('.match').length == legends.length){
-                alert('You Win!');
-            }
-            
-            Array.from(document.querySelectorAll('.card')).forEach(card => {
-                card.onclick = arguments.callee;
-            });
         },500);
     };
 
